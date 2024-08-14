0cd47afb4a6804739774110b1bcd9cb781e2eef0
Codezigineer <iamjojozm@gmail.com>
2024-08-13 17:39:49 -0700

    Initial code

---
+++ src/bvhStorage.ts
@@ -0,0 +1,36 @@
+import { MeshBVH } from "three-mesh-bvh";
+import { Mesh, SkinnedMesh } from 'three';
+
+export class BVHStorage
+{
+    meshes: { mesh: Mesh, bvh: MeshBVH } [];
+    riggedMeshes: { mesh: SkinnedMesh, bvh: MeshBVH } [];
+    
+    constructor(meshes: [ SkinnedMesh | Mesh ])
+    {
+        for(const mesh of meshes)
+        {
+            if(!(mesh as any instanceof Mesh)) continue;
+            const bvh = new MeshBVH(mesh.geometry);
+            const meshStore = { 
+                mesh: mesh, 
+                bvh
+            };
+            if(mesh instanceof SkinnedMesh)
+                this.riggedMeshes.push(meshStore as { mesh: SkinnedMesh, bvh: MeshBVH });
+            else this.meshes.push(meshStore);
+        };
+    };
+
+    updateRiggedMeshes()
+    {
+        for(var i = 0; i != this.riggedMeshes.length; i++)
+            this.riggedMeshes[i].bvh = new MeshBVH(this.riggedMeshes[i].mesh.geometry);
+    };
+
+    updateMeshes()
+    {
+        for(var i = 0; i != this.meshes.length; i++)
+            this.meshes[i].bvh = new MeshBVH(this.meshes[i].mesh.geometry);
+    };
+};
